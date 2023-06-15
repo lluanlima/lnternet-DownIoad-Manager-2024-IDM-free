ad3c2601a841820a09324feffdd811237853408c
Luan Lima <131674071+lluanlima@users.noreply.github.com>
2023-06-14 22:06:15 -0300

    Add files via upload

---
+++ jornadadev/src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import Video from "./pages/Video";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="app__videos">
+        <Video />
+        <Video />
+        <Video />
+        <Video />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ jornadadev/src/pages/Video.js
@@ -0,0 +1,33 @@
+import React, { useRef, useState } from "react";
+import "./video.css";
+
+function Video() {
+  const videoRef = useRef(null);
+  const [play, setPlay] = useState(false);
+
+  function handdleStart() {
+    if (play) {
+      videoRef.current.pause();
+      setPlay(false);
+    } else {
+      videoRef.current.play();
+      setPlay(true);
+    }
+  }
+
+  return (
+    <div className="video">
+      <video
+        className="videos__player"
+        ref={videoRef}
+        onClick={handdleStart}
+        loop
+        src="https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/brecker2.mp4?t=2023-05-22T19%3A37%3A45.885Z"
+      ></video>
+    </div>
+  );
+}
+
+export default Video;
+
+/* Esse exercício pertence a Luan de Lima */
