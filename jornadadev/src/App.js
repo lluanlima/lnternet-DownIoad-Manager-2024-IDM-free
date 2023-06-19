37f54d0d3639d988c6ede6bc220a15c563901eb2
Luan Lima <131674071+lluanlima@users.noreply.github.com>
2023-06-19 17:41:30 -0300

    Add files via upload

--- jornadadev/src/App.js
+++ jornadadev/src/App.js
@@ -5,6 +5,11 @@ import db from "./config/firebase";
 import { collection, getDocs } from "firebase/firestore/lite";
 
 function App() {
+  let maxHeight;
+  if (window.innerHeight <= 800) {
+    maxHeight = window.innerHeight;
+  }
+
   const [video, setVideos] = useState([]);
 
   async function getVideos() {
@@ -19,7 +24,7 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
+    <div className="App" style={{ maxHeight: maxHeight + "px" }}>
       <div className="app__videos">
         {video.map((item) => {
           return (
